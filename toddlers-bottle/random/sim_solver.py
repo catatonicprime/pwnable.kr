b7c4aacaf1d401f47db5de4164e95ba81f71258c
catatonicprime <catatonicprime@gmail.com>
2025-06-01 05:13:23 +0000

    Adding a sim_solver that does _not_ work. We solved this one by running gdb & getting the result of the rand in $rax. I'm guessing the libc rand hook angr uses is a different implementation.

---
+++ toddlers-bottle/random/sim_solver.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import angr
+import claripy
+import pwn
+import argparse
+
+parser = argparse.ArgumentParser(description="Select a filename.")
+parser.add_argument("filename", type=str, help="The name of the file to process")
+args = parser.parse_args()
+
+# Load the binary for analysis.
+proj = angr.Project(f'{args.filename}', auto_load_libs=False)
+proj.loader
+
+cfg = proj.analyses.CFGEmulated()
+start = cfg.functions['main']
+
+# Establish a state for solving
+state = proj.factory.entry_state(addr=start.addr, args=[f'{args.filename}'])
+
+# Create a simulation mamanger & find a path to the target.
+simgr = proj.factory.simgr(state)
+
+# Setup our target
+faddr = cfg.functions['main'].addr
+simgr.explore(find=[faddr+88], avoid=[faddr+166])
+
+pathcount = len(simgr.found)
+print("Length simgr.found: {}".format(pathcount))
+if pathcount == 0:
+    exit()
+
+# Show the solution!
+solution = simgr.found[0]
+print(solution.posix.dumps(0))
