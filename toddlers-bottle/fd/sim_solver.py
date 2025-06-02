b2bf884250bfa34f605f7e921d5dcdc42de6e185
catatonicprime <catatonicprime@gmail.com>
2025-06-02 18:22:28 +0000

    Updating the sim_solver to be much more dynamic in its solution generation.

--- toddlers-bottle/fd/sim_solver.py
+++ toddlers-bottle/fd/sim_solver.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
 import angr
 import claripy
-from pwnlib.util.packing import pack, flat
+import pwn
+import os
 import argparse
+
+parser = argparse.ArgumentParser(description='Select a filename.')
+parser.add_argument('filename', type=str, help='The name of the file to process')
+args= parser.parse_args()
 
 """
 Walk through:
@@ -11,57 +17,52 @@ Walk through:
     b. buf is a string read from 'fd'
     c. buf should be read from stdin (when fd equals 0)
 3. if strcmp of buf matches input then win.
-
-Early attempts to solve this using angr resulted in minor errors.
-1. Angr is not environmentally aware, so it makes guesses about
-   things like open files. A read() of an open file, which angr
-   automatically creates, with a file handle that computes to the
-   fd of the simfile - will satisfy this problem.
-2. Symbolically this is still correct. But concretely it needs a
-   little help. So we opt to catch the result of atoi() and
-   constraint it to 0x00.
 """
 
 # Load the binary for analysis.
-proj = angr.Project("./fd")
+proj = angr.Project(f'{args.filename}', auto_load_libs=False)
 proj.loader
-# cfg = proj.analyses.CFGEmulated()
-# start = cfg.functions['_start']
+
+cfg = proj.analyses.CFGEmulated()
+read_addr = [addr for addr, func in cfg.kb.functions.items() if 'read' == func.name and func.is_plt][0]
+
+def read_check(state):
+    if state.ip.args[0] == read_addr:
+        return True
 
 # Establish a state for solving
-passcode = claripy.BVS('passcode', 20*8)
-state_atoi = proj.factory.full_init_state(args=['./fd', passcode])
- 
-sim_atoi = proj.factory.simgr(state_atoi)
-atoi_result = sim_atoi.explore(find=[0x080484d0], avoid=[0x080484a5])
-assert len(atoi_result.found) > 0
+arg1_list = [claripy.BVS(f'arg1_0', 8)]
+arg1 = arg1_list[-1]
+for i in range(1,11):
+    arg1_list.append(claripy.BVS(f'arg1_{i}', 8))
+    arg1 = arg1.concat(arg1_list[-1])
 
-atoi_result.found[0].solver.add(atoi_result.found[0].regs.eax == 0x00)
-sim_stdin = proj.factory.simgr(atoi_result.found[0])
-sim_stdin.explore(find=[0x08048524], avoid=[0x08048548])
+state = proj.factory.entry_state(addr=cfg.functions['_start'].addr, args=[f'{args.filename}', arg1]) # 11 bytes for atoi
+simgr = proj.factory.simulation_manager(state)
 
-constraints = []
-# constraints.append(passcode.chop(bits=8)[3] == 0x30)
+# Find a state that is at the read
+simgr.explore(find=read_check)
 
-# Add constraints on inputs to ensure alpha-only characters are used.
-# for byte in passcode.chop(bits=8):
-#     constraints.append(byte >= 0x30) # minimum value
-#     constraints.append(byte <= 0x39) # maximum value
+if len(simgr.found) == 0:
+    print("No paths found")
+    exit(-1)
 
+found = simgr.found[0]
 
-# Add all the constraints to the solver.
-for constraint in constraints:
-    state.solver.add(constraint)
+# Constrain the fd memory location to be 0 for that state
+found.add_constraints(found.memory.load(found.regs.ebp.args[0] - 0x1c, 2) == 0)# "push DWORD PTR [ebp-0x1c]" immediately prior to read call
 
-simgr = proj.factory.simgr(state)
-simgr.explore(find=[0x08048524], avoid=[0x08048548, 0x080484a5])
+simgr.explore(find=read_check)
 
-pathcount = len(simgr.found)
-print("Length simgr.found: {}".format(pathcount))
-if pathcount == 0:
-    exit()
+if len(simgr.found) == 0:
+    print("No constrained paths found")
+    exit(-1)
 
-# Show the solution!
-solution = sim_stdin.found[0]
-print(solution.solver.eval(passcode, cast_to=bytes))
-print(solution.posix.stdin.concretize())
+solution = ''.join([chr(simgr.found[0].solver.eval(arg1_byte)) for arg1_byte in arg1_list])
+print(f'''
+import pwn
+p = pwn.process(['{args.filename}', '{solution}'])
+p.sendline('LETMEWIN')
+print(p.recvall().decode('utf-8'))
+p.close()
+''')
